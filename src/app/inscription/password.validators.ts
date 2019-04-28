5814a6eb37c5821345bc6eca7948d2ad03969413
MAHDI <bouchraramehdi@gmail.com>
2019-04-28 13:02:11 +0100

    password validator

---
+++ src/app/inscription/password.validators.ts
@@ -0,0 +1,15 @@
+import {AbstractControl} from '@angular/forms';
+
+export class PasswordValidator {
+
+    static passwordShouldMatch(control :AbstractControl){
+        let password = control.get('password');
+        let rpassword = control.get('rpassword');
+        if (password.value != rpassword.value){
+
+            return {passwordShouldMatch:true};
+        
+        return false;
+
+    }
+}
